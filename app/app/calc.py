9448268b1d67e11256ff2c12f58bcec59d866964
ericfei306 <eric0521bigjohn@gmail.com>
2023-08-11 18:59:30 +0800

    remove space to pass lint

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,13 +1,11 @@
-"""
-calculate function 
-"""
+"""calculate function"""
 
 
 def add(x, y):
-    """Add number x and y , and return the result"""
+    # """Add number x and y , and return the result"""
     return x + y
 
 
 def subtract(x, y):
-    """Subtract number x and y , and return the result"""
+    # """Subtract number x and y , and return the result"""
     return x - y
--- app/app/test.py
+++ app/app/test.py
@@ -1,22 +1,21 @@
-"""
-Sample tests
-"""
+# Sample tests
+
 from django.test import SimpleTestCase
 
 from app import calc
 
 
 class CalcTests(SimpleTestCase):
-    """Test the calc module."""
+    # """Test the calc module."""
 
     def test_add_numbers(self):
-        """Test adding numbers together."""
+        # """Test adding numbers together."""
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        """Test subtract numbers"""
+        # """Test subtract numbers"""
         res = calc.subtract(11, 5)
 
         self.assertEqual(res, 6)
