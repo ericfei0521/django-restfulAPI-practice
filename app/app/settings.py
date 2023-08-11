ab0868cd1088c68546c66ff43142be4e0ca6f649
ericfei306 <eric0521bigjohn@gmail.com>
2023-08-11 18:50:34 +0800

    fix conflicts

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,11 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-<<<<<<< HEAD
-SECRET_KEY = 'django-insecure-@xd(bu_vb#=xt3p430%xdc(5+ig41omuw2%uz-wc1x6em4rilc'
-=======
 SECRET_KEY = 'django-insecure-tw3@**b1p(dm3-4u)f%^u*s&ud!=m$0sj$-)neudat26x&z0hi'
->>>>>>> 0311db16590ae71d69821f0921b6d86c99c27834
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
