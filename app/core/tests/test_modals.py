bfe27d2c0538bd96a2c4ab8db25f98b125376bef
ericfei306 <eric0521bigjohn@gmail.com>
2023-08-23 19:49:11 +0800

    add authentication modal and normalize

---
+++ app/core/tests/test_modals.py
@@ -0,0 +1,29 @@
+"""TEST FOR MODALS"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModalTests(TestCase):
+    """Test Models"""
+
+    def test_create_user_with_email_and_password(self):
+        """Test creating user with an email & password"""
+        email = "test@example.com"
+        password = "testpass123"
+        user = get_user_model().objects.create_user(email=email, password=password)
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users"""
+        sample_emails = [
+            ["test1@EXAMPLE.com", "test1@example.com"],
+            ["Test2@Example.com", "Test2@example.com"],
+            ["TEST3@EXAMPLE.com", "TEST3@example.com"],
+            ["test4@example.COM", "test4@example.com"],
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, "sample123")
+            self.assertEqual(user.email, expected)
